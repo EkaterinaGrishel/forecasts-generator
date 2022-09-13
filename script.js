8c1ec450b1e1407db7ea9b59f8c3bdb634b66986
EkaterinaGrishel <katesobol88@gmail.com>
2022-09-13 16:50:30 +0300

    Выполнила hometask-5

--- script.js
+++ script.js
@@ -13,3 +13,50 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+const forecastButton = document.querySelector('.forecast-btn');
+const forecastsLast = document.querySelector('.forecasts');
+const forecastItemTemplate = document.querySelector('#forecast-item');
+const currentForecastTitle = document.querySelector('.current-forecast h1');
+const currentForecastPercent = document.querySelector('.current-forecast p');
+
+const forecast1 = "Всё будет хорошо!";
+const forecast2 = "Ты найдёшь вещь, которую потеряла!";
+const forecast3 = "Тебя ждёт умопомрачительный вечер!";
+const forecast4 = "Вечером тебя ожидает приятный сюрприз!";
+
+const generateRandomValue = (min, max) => {
+  return +((Math.random() * (max - min)) + min).toFixed(0);
+};
+
+
+const makeItemTemplate = (title, percent) => {
+  const lastItemForecast = forecastItemTemplate.content.cloneNode(true);
+  lastItemForecast.querySelector('h3').textContent = title;
+  lastItemForecast.querySelector('p').textContent = percent;
+  if (title !== '' && percent !== '') {
+  forecastsLast.prepend(lastItemForecast);
+}
+  return lastItemForecast;
+};
+
+forecastButton.addEventListener('click', function() {
+const forecast = generateRandomValue(1, 4);
+const percent = generateRandomValue(1, 100);
+makeItemTemplate(currentForecastTitle.textContent, currentForecastPercent.textContent);
+
+currentForecastPercent.textContent = `Вероятность: ${percent}%`;
+
+ if (forecast  === 1) {
+ currentForecastTitle.textContent = forecast1;
+ }  
+ else if (forecast  === 2) {
+ currentForecastTitle.textContent = forecast2;
+ }
+ else if (forecast  === 3) {
+  currentForecastTitle.textContent = forecast3;
+ }
+ else {
+ currentForecastTitle.textContent = forecast4;
+ }
+});
